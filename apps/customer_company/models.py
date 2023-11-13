706613ea6404497078b5860a0fd5a43916daf4ed
Adi <sp.adi.kg@gmail.com>
2023-11-13 19:18:24 +0600

    first commit

--- apps/customer_company/models.py
+++ apps/customer_company/models.py
@@ -1,9 +1,16 @@
+# models.py
+
 from django.db import models
 
-
 class CustomerCompany(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    website = models.URLField(blank=True)
+    description = models.TextField(blank=True)
+    industry = models.CharField(max_length=100, blank=True)
+    logo = models.ImageField(upload_to='company_logos/', null=True, blank=True)
 
-    company_name = models.CharField()
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=50, unique=True)
-    
+    def __str__(self):
+        return self.name
